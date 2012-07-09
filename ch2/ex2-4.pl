3c5994adfb68ecb63a8d6cf04612b33c14fbffa7
Jeho, Sung <jeho.sung@silex.kr>
2012-07-09 16:48:49 +0900

    ex2-4 연습문제
    프롬프트에서 숫자 두개를 별도의 줄에서 입력을 읽어 들인 후, 두개의
    숫자를 곱한 값을 출력하는 프로그램을 작성하세요

---
+++ ch2/ex2-4.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+
+=pod
+
+=encoding utf-8
+
+=head1 ch2-4 연습문제
+
+프롬프트에서 숫자 두개를 별도의 줄에서 입력을 읽어 들인 후, 
+두개의 숫자를 곱한 값을 출력하는 프로그램을 작성하세요
+
+=cut
+
+say <STDIN> * <STDIN>;
